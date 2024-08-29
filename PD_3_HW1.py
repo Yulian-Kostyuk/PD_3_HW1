3cf2f28586cea0c2fccd33a8e1d774b57f12b644
Yulian-Kostyuk <122096456+Yulian-Kostyuk@users.noreply.github.com>
2024-08-29 14:51:47 +0300

    Add files via upload
    Цель: применить на практике начальные знания о пространстве имён и оператор global. Закрепить навыки из предыдущих модулей.
    
    Задача "Счётчик вызовов":
    Порой необходимо отслеживать, сколько раз вызывалась та или иная функция. К сожалению, в Python не предусмотрен подсчёт вызовов автоматически.
    Давайте реализуем данную фишку самостоятельно!
    
    Вам необходимо написать 3 функции:
    Функция count_calls подсчитывающая вызовы остальных функций.
    Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
    Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
    Пункты задачи:
    Создать переменную calls = 0 вне функций.
    Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
    Создать функцию string_info с параметром string и реализовать логику работы по описанию.
    Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
    Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
    Вывести значение переменной calls на экран(в консоль).
    
    Пример результата выполнения программы:
    Пример выполняемого кода:
    print(string_info('Capybara'))
    print(string_info('Armageddon'))
    print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
    print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
    print(calls)
    Вывод на консоль:
    (8, 'CAPYBARA', 'capybara')
    (10, 'ARMAGEDDON', 'armageddon')
    True
    False
    4
    
    Примечания:
    Для использования глобальной переменной внутри функции используйте оператор global.
    Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.

---
+++ PD_3_HW1.py
@@ -0,0 +1,31 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    a = (len(string), string.upper(), string.lower())
+    count_calls()
+    return a
+
+list_to_search = []
+def is_contains(string, list_to_search):
+    b = []
+    for i in range(len(list_to_search)):
+        if list_to_search[i].lower() in string.lower():
+            a = True
+            b.append(a)
+        else:
+            a = False
+            b.append(a)
+    count_calls()
+    if True in b:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
